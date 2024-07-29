2abacdfe38495de89dcc6434d73a0c490dc2aae9
areyshka <elena.draven@mail.ru>
2024-07-29 18:31:46 +0300

    feat: add solution of reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  let result = '';
+  if (n > 0) {
+    result = str.split('').reverse().join('');
+  } else {
+    result = str.slice(1).split('').reverse().join('');
+  }
+  return +result;
 }
+
